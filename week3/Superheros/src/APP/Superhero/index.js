25e7d506ad1a51ca1bf5c1bdc405dda24e5362c7
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-06-20 18:08:04 -0400

    Week3
    Superhero

---
+++ week3/Superheros/src/APP/Superhero/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+
+class Superhero extends Component {
+    constructor(props) {
+        super(props);
+        this.superhero = {
+            ...props
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.superhero.displayCatchPhrase(this.superhero.catchphrase);
+    }
+    
+    render() {
+        return (
+            <div onClick={this.handleClick} className='superhero-wrapper'>
+                <h1 className='name'>{this.superhero.name}</h1>
+                <img className='image' src={this.superhero.imgUrl} alt={`Photo of ${this.superhero.name}`}/>
+            </div>
+        );
+    }
+}
+
+export default Superhero;
---
+++ week3/Superheros/src/APP/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import superheros from './superheros';
+import Superhero from './Superhero';
+import './index.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.displayCatchPhrase = this.displayCatchPhrase.bind(this);
+    }
+    displayCatchPhrase(str) {
+        alert(str);
+    }
+    render() {
+        return (
+        <div className="app-wrapper">
+            {superheroes.map((superhero, index) => {
+                return (
+                        <Superhero displayCatchPhrase={this.displayCatchPhrase} {...superhero} key={index}></Superhero>
+                );
+            })}
+        </div>
+        );
+        
+    }
+}
+
+export default App;
