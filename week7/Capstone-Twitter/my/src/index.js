cdb3bda6ae80bae45828bef4bfcc93060c2d500f
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-07-21 07:39:24 -0400

    Week7

--- week7/Capstone-Twitter/my/src/index.js
+++ week7/Capstone-Twitter/my/src/index.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom"
-import App from './App';
+import React, { Component } from 'react';
+import { BackHandler } from 'react-native';
+import { connect } from 'react-redux';
+import { addNavigationHelpers } from 'react-navigation';
+import AppNavigator from './NavigationStack';
+import { addListener } from './Utils/redux';
+import { NavigationActions } from 'react-navigation';
+// import { logout } from "../Actions/actionCreator";
 
+// navigation component
+class AppNavigation extends Component {
+  
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />,
-    </BrowserRouter>, document.getElementById('root'));
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  onBackPress = () => {
+    const { dispatch, navigationState } = this.props;
+    if (navigationState.stateForLoggedIn.index <= 1) {
+      BackHandler.exitApp();
+      return;
+    }
+    dispatch(NavigationActions.back());
+    return true;
+  };
+
+  render() {
+    const { navigationState, dispatch, isLoggedIn } = this.props;
+    const state = isLoggedIn
+      ? navigationState.stateForLoggedIn
+      : navigationState.stateForLoggedOut;
+
+    return (
+      <AppNavigator
+        navigation={addNavigationHelpers({
+          dispatch,
+          state,
+          addListener,
+        })}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isLoggedIn: state.LoginReducer.isLoggedIn,
+    navigationState: state.NavigationReducer,
+  };
+};
+
+export default connect(mapStateToProps)(AppNavigation);
